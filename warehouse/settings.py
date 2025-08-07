db384e444681f73b40b150876f2638e9658fcea1
sahil-clould <sahilgaonkar0909@gmail.com>
2025-08-07 16:43:16 +0530

    Added the tekplate

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'warehouse.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'inventory' / 'Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
